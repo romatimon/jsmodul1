1d4ec04cf974ae9242c89b1b60eca26c9e44c4bc
Roman <example@gmail.com>
2023-01-31 20:14:55 +0300

    script

--- js/script.js
+++ js/script.js
@@ -84,18 +84,73 @@
 // }
 
 
-// const hamburger = 0;
-// const fries = null;
+// const hamburger = 3;
+// const fries = 3;
 // const cola = 0;
+// const nuggets = 2;
 
-// if (hamburger || cola || fries) {
+// if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
 //     console.log("Все довольны!");
 // } else {
 //     console.log("Мы уходим");
 // }
 
-// console.log(hamburger || cola || fries);
+// console.log(hamburger === 3 && cola === 2 || fries === 3 && nuggets);
 
-let johnReport, alexReport, samReport, mariaReport = 'done';
+// let johnReport, alexReport, samReport, mariaReport = 'done';
 
-console.log(johnReport || alexReport || samReport || mariaReport);
+// console.log(johnReport || alexReport || samReport || mariaReport);
+
+// console.log(!0);
+
+// console.log(NaN || 2 || undefined);
+
+// console.log( NaN && 2 && undefined );
+
+// console.log( 1 && 2 && 3 );
+
+// console.log( 25 || null && !3 );
+
+// console.log( NaN || null || !3 || undefined || 5);
+
+// console.log( NaN || null && !3 && undefined || 5);
+
+// console.log( 5 === 5 && 3 > 1 || 5);
+
+
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+
+// if (hamburger === 3 && cola || fries === 3 && nuggets) {
+//    console.log('Done!')
+// }
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+
+
+// if (hamburger || cola || fries === 3 || nuggets) {
+//    console.log('Done!')
+// }
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+
+
+// if (hamburger && cola || fries === 3 && nuggets) {
+//    console.log('Done!')
+// }
+
+
+let num = 50;
+
+while (num < 55) {
+    console.log(num);
+    num++;
+}
